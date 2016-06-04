82253c5becfa4191827ae201fcdcd5bfdb20f447
zevas <mr.vadimvv@mail.ru>
2016-06-05 01:54:42 +0300

    .

---
+++ Algorithms/MyQueue/MyQueue/MyQueue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+
+namespace MyQueue
+{
+    class MyQueue<T> 
+    {
+        private readonly Stack<T> Top;
+        private readonly Stack<T> Tail;
+        private T newElement;
+        public MyQueue()
+        {
+            Top = new Stack<T>();
+            Tail = new Stack<T>();
+        }
+
+        public void Push(T element)
+        {
+            newElement = element;
+            ToQueue();
+        }
+
+        public void ToQueue()
+        {
+            while(Top.Count!=0)
+            {
+                Tail.Push(Top.Pop());
+            }
+            Tail.Push(newElement);
+
+            while (Tail.Count != 0)
+            {
+                Top.Push(Tail.Pop());
+            }
+
+        }
+
+        public void Pop()
+        {
+            Top.Pop();
+        }
+
+        public T Peek()
+        {
+            return Top.Peek();
+        }
+    }
+}
---
+++ Algorithms/MyQueue/MyQueue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace MyQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyStack = new MyQueue<int>();
+
+            for (int i = 0; i < 150; i++)
+            {
+                MyStack.Push(i);
+            }
+            for (int i = 0; i < 150; i++)
+            {
+                Console.WriteLine(MyStack.Peek());
+                MyStack.Pop();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
