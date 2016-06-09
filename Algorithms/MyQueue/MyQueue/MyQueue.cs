94931e0fda84e64acfa4b9563202244765b0a68c
zevas <mr.vadimvv@mail.ru>
2016-06-09 23:18:18 +0300

    remade

--- Algorithms/MyQueue/MyQueue/MyQueue.cs
+++ Algorithms/MyQueue/MyQueue/MyQueue.cs
@@ -5,44 +5,39 @@ namespace MyQueue
 {
     class MyQueue<T> 
     {
-        private readonly Stack<T> Top;
+        private readonly Stack<T> Head;
         private readonly Stack<T> Tail;
-        private T newElement;
         public MyQueue()
         {
-            Top = new Stack<T>();
+            Head = new Stack<T>();
             Tail = new Stack<T>();
         }
 
         public void Push(T element)
         {
-            newElement = element;
+            Tail.Push(element);
             ToQueue();
         }
 
         public void ToQueue()
         {
-            while(Top.Count!=0)
+            if (Head.Count != 0) return;
+            while (Tail.Count>0)
             {
-                Tail.Push(Top.Pop());
+                Head.Push(Tail.Pop());
             }
-            Tail.Push(newElement);
-
-            while (Tail.Count != 0)
-            {
-                Top.Push(Tail.Pop());
-            }
-
         }
 
         public void Pop()
         {
-            Top.Pop();
+            Head.Pop();
+            ToQueue();
         }
 
         public T Peek()
         {
-            return Top.Peek();
+            
+            return Head.Peek();
         }
     }
 }
