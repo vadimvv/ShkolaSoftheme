80a844dd6dcb28e91623b57a94d094c4596a31ce
zevas <mr.vadimvv@mail.ru>
2016-06-11 14:22:45 +0300

    .

---
+++ CSharpHW/16/Collections/MyQueue/MyQueue/MyQueue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+
+namespace MyQueue
+{
+    class MyQueue<T> 
+    {
+        private readonly Stack<T> Head;
+        private readonly Stack<T> Tail;
+        public MyQueue()
+        {
+            Head = new Stack<T>();
+            Tail = new Stack<T>();
+        }
+
+        public void Push(T element)
+        {
+            Tail.Push(element);
+            ToQueue();
+        }
+
+        public void ToQueue()
+        {
+            if (Head.Count != 0) return;
+            while (Tail.Count>0)
+            {
+                Head.Push(Tail.Pop());
+            }
+        }
+
+        public void Pop()
+        {
+            Head.Pop();
+            ToQueue();
+        }
+
+        public T Peek()
+        {
+            
+            return Head.Peek();
+        }
+    }
+}
---
+++ CSharpHW/16/Collections/MyStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MyStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyStack")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f05a6c66-fa5b-4fbf-aae3-fcab71fe10ee")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
